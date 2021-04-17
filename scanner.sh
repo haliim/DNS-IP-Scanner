d86d97846fd5c2059af753fe53d23bf508d6428e
Ahmed Halim <ah.hesham93@gmail.com>
2021-04-17 15:41:02 +0200

    small changes

--- scanner.sh
+++ scanner.sh
@@ -21,4 +21,4 @@ done
 echo -e "Thank you, the scan has been completed \e[92mSuccessfuly! :) "
 echo -e "\e[0mPlease open \e[91mServersUp.txt \e[0mto find all servers that are working properly and open \e[91mServersDown.txt \e[0mto find all dead servers" 
 
-
+# sudo nmap -O -sV "$server" > output.txt
